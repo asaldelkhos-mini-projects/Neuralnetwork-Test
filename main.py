0e2fc9e2cdb32701597517ecc0733577e7a15d57
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-12 15:47:58 +0430

    init: main

---
+++ main.py
@@ -0,0 +1,11 @@
+# import the necessary packages
+from network import NeuralNetwork
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn import datasets
+
+
+
+if __name__ == "__main__":
+    
