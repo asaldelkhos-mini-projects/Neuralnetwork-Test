17a4a4a8fd9ade8eb90c5d38c6dc841677ccc2b5
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-12 15:49:14 +0430

    feat: loading dataset

--- main.py
+++ main.py
@@ -8,4 +8,17 @@ from sklearn import datasets
 
 
 if __name__ == "__main__":
-    
+    # load the MNIST dataset and apply min/max scaling to scale the
+    # pixel intensity values to the range [0, 1] (each image is
+    # represented by an 8 x 8 = 64-dim feature vector)
+    print("[INFO] loading MNIST (sample) dataset...")
+    digits = datasets.load_digits()
+    data = digits.data.astype("float")
+    data = (data - data.min()) / (data.max() - data.min())
+    print("[INFO] samples: {}, dim: {}".format(data.shape[0], data.shape[1]))
+
+    # construct the training and testing splits
+    (trainX, testX, trainY, testY) = train_test_split(data, digits.target, test_size=0.25)
+    # convert the labels from integers to vectors
+    trainY = LabelBinarizer().fit_transform(trainY)
+    testY = LabelBinarizer().fit_transform(testY)
